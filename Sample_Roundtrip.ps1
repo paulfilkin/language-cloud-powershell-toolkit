f12045fc43b4a5d9a3a29da63006b8d36f3088aa
aflorescu579774 <aflorescu@sdl.com>
2024-09-21 15:12:14 +0300

    SDLCOM-6137 - Updated the Sample Roundtrip

--- Sample_Roundtrip.ps1
+++ Sample_Roundtrip.ps1
@@ -21,6 +21,7 @@ Import-Module -Name AuthenticationHelper
 Import-Module -Name ProjectHelper
 Import-Module -Name ResourcesHelper
 Import-Module -Name UsersHelper
+Import-Module -Name TerminologyHelper
 
 # Retrieve the access key using provided credentials
 Write-Host "`nRetrieving access key..." -ForegroundColor Cyan
@@ -29,9 +30,25 @@ $accessKey = Get-AccessKey -id $clientId -secret $clientSecret -lcTenant $lcTena
 # Confirm that the access key was retrieved successfully
 Write-Host "Access key retrieved successfully." -ForegroundColor Green
 
+# List all locations
+Write-Host "`nListing all locations:" -ForegroundColor Cyan
+$locations = Get-AllLocations -accessKey $accessKey
+if ($locations) {
+    for ($i = 0; $i -lt $locations.Count; $i++) {
+        Write-Host "[$($i + 1)] $($locations[$i].name)" -ForegroundColor Green
+    }
+} else {
+    Write-Host "No locations found." -ForegroundColor Yellow
+}
+
+$rootLocation = $locations[0];
+
+# Wait 3 seconds after each retrieve to view the list
+Start-Sleep -Seconds 3;
+
 # List all users
-Write-Host "`nListing all users:" -ForegroundColor Cyan
-$users = Get-AllUsers -accessKey $accessKey
+Write-Host "`nListing all users from the root location:" -ForegroundColor Cyan
+$users = Get-AllUsers -accessKey $accessKey -locationId $rootLocation.Id
 if ($users) {
     for ($i = 0; $i -lt $users.Count; $i++) {
         Write-Host "[$($i + 1)] $($users[$i].email) - $($users[$i].firstName) $($users[$i].lastName)" -ForegroundColor Green
@@ -40,9 +57,12 @@ if ($users) {
     Write-Host "No users found." -ForegroundColor Yellow
 }
 
+# Wait 3 seconds after each retrieve to view the list
+Start-Sleep -Seconds 3;
+
 # List all groups
-Write-Host "`nListing all groups:" -ForegroundColor Cyan
-$groups = Get-AllGroups -accessKey $accessKey
+Write-Host "`nListing all groups from the root location:" -ForegroundColor Cyan
+$groups = Get-AllGroups -accessKey $accessKey -locationId $rootLocation.Id
 if ($groups) {
     for ($i = 0; $i -lt $groups.Count; $i++) {
         Write-Host "[$($i + 1)] $($groups[$i].name) - $($groups[$i].description)" -ForegroundColor Green
@@ -51,31 +71,82 @@ if ($groups) {
     Write-Host "No groups found." -ForegroundColor Yellow
 }
 
+# Wait 3 seconds after each retrieve to view the list
+Start-Sleep -Seconds 3;
+
 # List all project templates
-Write-Host "`nListing all project templates:" -ForegroundColor Cyan
-$projectTemplates = Get-AllProjectTemplates -accessKey $accessKey
+Write-Host "`nListing all project templates from the root location:" -ForegroundColor Cyan
+$projectTemplates = Get-AllProjectTemplates -accessKey $accessKey -locationId $rootLocation
 if ($projectTemplates) {
     for ($i = 0; $i -lt $projectTemplates.Count; $i++) {
-        Write-Host "[$($i + 1)] $($projectTemplates[$i].name) - $($projectTemplates[$i].description)" -ForegroundColor Green
+        Write-Host "[$($i + 1)] $($projectTemplates[$i].name)" -ForegroundColor Green
     }
 } else {
     Write-Host "No project templates found." -ForegroundColor Yellow
 }
 
+# Wait 3 seconds after each retrieve to view the list
+Start-Sleep -Seconds 3;
+
 # List all translation engines
-Write-Host "`nListing all translation engines:" -ForegroundColor Cyan
-$translationEngines = Get-AllTranslationEngines -accessKey $accessKey
+Write-Host "`nListing all translation engines from the root location:" -ForegroundColor Cyan
+$translationEngines = Get-AllTranslationEngines -accessKey $accessKey -locationId $rootLocation.Id
 if ($translationEngines) {
     for ($i = 0; $i -lt $translationEngines.Count; $i++) {
-        Write-Host "[$($i + 1)] $($translationEngines[$i].name) - $($translationEngines[$i].description)" -ForegroundColor Green
+        Write-Host "[$($i + 1)] $($translationEngines[$i].name)" -ForegroundColor Green
     }
 } else {
     Write-Host "No translation engines found." -ForegroundColor Yellow
 }
 
+# Wait 3 seconds after each retrieve to view the list
+Start-Sleep -Seconds 3;
+
+# List all file type configurations
+Write-Host "`nListing all file type configurations from the root location:" -ForegroundColor Cyan
+$fileTypeConfigs = Get-AllFileTypeConfigurations -accessKey $accessKey -locationId $rootLocation.Id
+if ($fileTypeConfigs) {
+    for ($i = 0; $i -lt $fileTypeConfigs.Count; $i++) {
+        Write-Host "[$($i + 1)] $($fileTypeConfigs[$i].name)" -ForegroundColor Green
+    }
+} else {
+    Write-Host "No file type configurations found." -ForegroundColor Yellow
+}
+
+# Wait 3 seconds after each retrieve to view the list
+Start-Sleep -Seconds 3;
+
+# List all workflows
+Write-Host "`nListing all workflows from the root location:" -ForegroundColor Cyan
+$workflows = Get-AllWorkflows -accessKey $accessKey -locationId $rootLocation.Id
+if ($workflows) {
+    for ($i = 0; $i -lt $workflows.Count; $i++) {
+        Write-Host "[$($i + 1)] $($workflows[$i].name)" -ForegroundColor Green
+    }
+} else {
+    Write-Host "No workflows found." -ForegroundColor Yellow
+}
+
+# Wait 3 seconds after each retrieve to view the list
+Start-Sleep -Seconds 3;
+
+# List all pricing models
+Write-Host "`nListing all pricing models from the root location:" -ForegroundColor Cyan
+$pricingModels = Get-AllPricingModels -accessKey $accessKey -locationId $rootLocation.Id
+if ($pricingModels) {
+    for ($i = 0; $i -lt $pricingModels.Count; $i++) {
+        Write-Host "[$($i + 1)] $($pricingModels[$i].name)" -ForegroundColor Green
+    }
+} else {
+    Write-Host "No pricing models found." -ForegroundColor Yellow
+}
+
+# Wait 3 seconds after each retrieve to view the list
+Start-Sleep -Seconds 3;
+
 # List all customers
-Write-Host "`nListing all customers:" -ForegroundColor Cyan
-$customers = Get-AllCustomers -accessKey $accessKey
+Write-Host "`nListing all customers from the rootLocation:" -ForegroundColor Cyan
+$customers = Get-AllCustomers -accessKey $accessKey -locationId $rootLocation.Id
 if ($customers) {
     for ($i = 0; $i -lt $customers.Count; $i++) {
         Write-Host "[$($i + 1)] $($customers[$i].name) - $($customers[$i].location)" -ForegroundColor Green
@@ -84,92 +155,85 @@ if ($customers) {
     Write-Host "No customers found." -ForegroundColor Yellow
 }
 
-# List all file type configurations
-Write-Host "`nListing all file type configurations:" -ForegroundColor Cyan
-$fileTypeConfigs = Get-AllFileTypeConfigurations -accessKey $accessKey
-if ($fileTypeConfigs) {
-    for ($i = 0; $i -lt $fileTypeConfigs.Count; $i++) {
-        Write-Host "[$($i + 1)] $($fileTypeConfigs[$i].name) - $($fileTypeConfigs[$i].location)" -ForegroundColor Green
-    }
-} else {
-    Write-Host "No file type configurations found." -ForegroundColor Yellow
+# Wait 3 seconds after each retrieve to view the list
+Start-Sleep -Seconds 3;
+
+Write-Host "`nCreating a new customer" -ForegroundColor Cyan
+$customerName = "PowerShell Customer Sample"
+
+$newCustomer = New-Customer -accessKey $accessKey -customerName $customerName -locationId $rootLocation.Id
+if ($newCustomer)
+{
+    Write-Host "`nCutomer [$($newCustomer.Name)] created." -ForegroundColor Green
+    Write-Host "`nUpdating the rag status" -ForegroundColor Cyan;
+    Update-Customer -accessKey $accessKey -customerId $newCustomer.Id -ragStatus "red";
 }
 
-# List all workflows
-Write-Host "`nListing all workflows:" -ForegroundColor Cyan
-$workflows = Get-AllWorkflows -accessKey $accessKey
-if ($workflows) {
-    for ($i = 0; $i -lt $workflows.Count; $i++) {
-        Write-Host "[$($i + 1)] $($workflows[$i].name) - $($workflows[$i].description)" -ForegroundColor Green
+Write-Host "`nManaging Translation Memories from the root location." -ForegroundColor Cyan;
+Write-Host "`nListing all the existing Translation Memories" -ForegroundColor Cyan;
+$tms = Get-AllTranslationEngines -accessKey $accessKey -locationId $rootLocation.Id;
+if ($tms) 
+{
+    for ($i = 0; $i -lt $tms.Count; $i++)
+    {
+        Write-Host "[$($i + 1)] $($tms[$i].name)" -ForegroundColor Green
     }
-} else {
-    Write-Host "No workflows found." -ForegroundColor Yellow
 }
 
-# List all pricing models
-Write-Host "`nListing all pricing models:" -ForegroundColor Cyan
-$pricingModels = Get-AllPricingModels -accessKey $accessKey
-if ($pricingModels) {
-    for ($i = 0; $i -lt $pricingModels.Count; $i++) {
-        Write-Host "[$($i + 1)] $($pricingModels[$i].name) - $($pricingModels[$i].description)" -ForegroundColor Green
+# Wait 3 seconds after each retrieve to view the list
+Start-Sleep -Seconds 3;
+
+Write-Host "`nCreating a new translation memory" -ForegroundColor Cyan;
+$languageProcessingRule = Get-AllLanguageProcessingRules -accessKey $accessKey | Select-Object -First 1;
+$fieldTemplate = Get-AllFieldTemplates -accessKey $accessKey | Select-Object -First 1;
+if ($languageProcessingRule -and $fieldTemplate)
+{
+    $newTM = New-TranslationMemory -accessKey $accessKey -name "Powershell Translation Memory" `
+                -languageProcessingIdOrName $languageProcessingRule.Id -fieldTemplateIdOrName $fieldTemplate.Id `
+                -sourceLanguage "en-US" -targetLanguages @("de-DE")
+
+    if ($newTM)
+    {
+        Write-Host "`nTranslation Memory [$($newTM.Name)] created" -ForegroundColor Green;
     }
-} else {
-    Write-Host "No pricing models found." -ForegroundColor Yellow
 }
 
-# List all schedule templates
-Write-Host "`nListing all schedule templates:" -ForegroundColor Cyan
-$scheduleTemplates = Get-AllScheduleTemplates -accessKey $accessKey
-if ($scheduleTemplates) {
-    for ($i = 0; $i -lt $scheduleTemplates.Count; $i++) {
-        Write-Host "[$($i + 1)] $($scheduleTemplates[$i].name) - $($scheduleTemplates[$i].description)" -ForegroundColor Green
-    }
-} else {
-    Write-Host "No schedule templates found." -ForegroundColor Yellow
+Write-Host "`nCreating a project template" -ForegroundColor Cyan;
+# Initialize the dependencies to be used for project template creation and project creation
+$fileTypeConfiguration = $fileTypeConfigs[0]
+$translationEngine = $translationEngines[0]
+$workflow = $workflows[0]
+
+# Create the project Template
+$newProjectTemplate = New-ProjectTemplate -accessKey $accessKey -projectTemplateName "Powershell Project Template" `
+                -locationId $rootLocation.Id -fileTypeConfigurationIdOrName $fileTypeConfiguration.Id `
+                -translationEngineIdOrName $translationEngine.Id -workflowIdOrName $workflow.Id `
+                -sourceLanguage "en-US" -targetLanguages @("de-DE");
+
+if ($newProjectTemplate)
+{
+    Write-Host "Project Template [$($newProjectTemplate.Name)] created" -ForegroundColor Green;
 }
 
-# List all locations
-Write-Host "`nListing all locations:" -ForegroundColor Cyan
-$locations = Get-AllLocations -accessKey $accessKey
-if ($locations) {
-    for ($i = 0; $i -lt $locations.Count; $i++) {
-        Write-Host "[$($i + 1)] $($locations[$i].name) - $($locations[$i].location)" -ForegroundColor Green
-    }
-} else {
-    Write-Host "No locations found." -ForegroundColor Yellow
-}
+Write-Host "`nCreating a new project" -ForegroundColor Cyan;
 
+# Creates a file in the script location for project creation
+$scriptDirectory = $PSScriptRoot
+$fileName = "sample_text_file.txt"
+$fileContent = "This is a sample text file created by PowerShell."
+$filePath = Join-Path $scriptDirectory $fileName
+Set-Content -Path $filePath -Value $fileContent
 
-# Create a sample text file with "Hello, World!" content
-$filePath = Join-Path -Path (Get-Location) -ChildPath "HelloWorld.txt"
-Set-Content -Path $filePath -Value "Hello, World!"
-Write-Host "`nSample text file created at: $filePath" -ForegroundColor Cyan
+# declare the basic data.
+$dueDate = "2025-03-24"
+$dueTime = "12:00"
+$workflow = $workflows[0]
 
-# Get the due date 7 days from now and format it to YYYY-MM-DDTHH-MMZ
-$dueDate = (Get-Date).AddDays(7)
-$projectDueDate = $dueDate.ToString("yyyy-MM-ddTHH:mmZ")  # Format to desired string
-c
-# Define project parameters
-$projectName = "Sample Project"
-$projectDueDate = $projectDueDate
-$sourceLanguage = "en-US"
-$targetLanguages = @("de-DE", "fr-FR")
-$location = $locations[0].id
-$description = "This is a sample project created via PowerShell script."
-$translationEngine = $translationEngines[0].name  # Use the first translation engine
-$workflow = $workflows[0].id # Use the first workflow
-$fileTypeConfiguration = $fileTypeConfigs[0].id # Use the first file type configuration
-$filesPath = $filePath  # Use the current directory for files
-
-Write-Host $pricingModel;
-
-# Create the project using New-Project function
-Write-Host "`nCreating new project..." -ForegroundColor Green
-New-Project -accessKey $accessKey -projectName $projectName -projectDueDate $projectDueDate `
-    -filesPath $filesPath -referenceFileNames @() -sourceLanguage $sourceLanguage `
-    -targetLanguages $targetLanguages -location $location `
-    -description $description -workflow $workflow `
-    -translationEngine $translationEngine -fileProcessingConfiguration $fileTypeConfiguration
+# Create the project
+new-project $accessKey -name "PowerShell Project" -dueDate $dueDate -dueTime $dueTime `
+        -fileTypeConfigurationIdOrName $fileTypeConfiguration.Id -translationEngineIdOrName $translationEngine.id `
+        -filesPath $filePath -locationId $rootLocation.Id `
+        -sourceLanguage "en-US" -targetLanguages @("de-DE") -workflowIdOrName $workflow.Id
 
 # Display script completion message with decorative separators
 Write-Host "`n======================================" -ForegroundColor Yellow
